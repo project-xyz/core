a92cacbcc5a93ea48be5caa9897aea26d8b4eee0
Steven Liu <steven.jieliu91@gmail.com>
2013-08-11 11:06:49 +0100

    Revert "update"
    This reverts commit 370dd1b8146999c3b50a012e2cbfaba22f15e501.
    
    Conflicts:
    	application/config/database.php
    	application/controllers/main.php
    	application/views/forms/login.php
    	application/views/forms/sign_up.php

--- application/views/footer.php
+++
@@ -1,9 +0,0 @@
-
-
-<script src="<?php echo base_url(); ?>static/lib/jquery/jquery.js"></script>
-<script src="<?php echo base_url(); ?>static/lib/bootstrap/js/bootstrap.js"></script>
-
-<script src="<?php echo base_url(); ?>static/lib/jvalidation/validation.js"></script>
-<script src="<?php echo base_url(); ?>static/js/forms/form_validation.js"></script>
-</body>
-</html>
